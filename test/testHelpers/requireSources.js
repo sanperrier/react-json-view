79fb8c6ba559ace18e7d562beaa2aeeb79b326c5
Vyacheslav Dorzhiev <aliencrowdman@yandex.ru>
2021-02-12 17:22:47 +0800

    update deps

--- test/testHelpers/requireSources.js
+++ test/testHelpers/requireSources.js
@@ -2,7 +2,7 @@ import register from "ignore-styles"
 register([".sass", ".scss"])
 import "./../../src/js/index"
 
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { configure } from 'enzyme';
 import jsdom from 'jsdom';
 
